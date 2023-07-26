3deb9d93636c593ebab5ff3cb6096879257fab20
Bastián Poloni <128436347+bastianpoloni@users.noreply.github.com>
2023-07-25 21:40:11 -0400

    commit final

--- app.js
+++ app.js
@@ -3,20 +3,52 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import yargs from "yargs";
 import hbs from "hbs";
+import Jimp from "jimp";
+import bodyParser from "body-parser";
+import fs from "fs";
+//import multer from "multer";
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const argv = yargs(process.argv.slice(2)).argv;
+//const upload = multer({  dest: "public/img",});
+let url = "";
 
 app.set("view engine", "hbs");
-//app.use(express.static('public'));
-app.use(express.static(__dirname + 'public'));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 hbs.registerPartials(__dirname + "/views/partials");
 
 app.get("/", (req, res) => {
-    res.render("index");
-})
+  res.render("index");
+});
 
-app.listen(3000, () => {
+app.post("/", async (req, res) => {
+  url = req.body.inputUrl;
+
+  try {
+    console.log(url);
+    const image = await Jimp.read(req.body.inputUrl);
+    const buffer = await image.getBufferAsync(Jimp.MIME_PNG);
+    image
+      .greyscale()
+      .quality(60)
+      .resize(350, Jimp.AUTO)
+      .write("public/img/newImg.jpg");
+      res.send(`<img src="/img/newImg.jpg">`);
+  } catch (error) {
+    console.log(error);
+  }
+  
+});
+
+
+//ingresar nodemon app.js --key 123 para que servidor levante
+if (argv.key == "123") {
+  app.listen(3000, () => {
     console.log("Servidor corriendo en el puerto 3000");
-})
+  });
+} else {
+  console.log("Key incorrecta");
+}
